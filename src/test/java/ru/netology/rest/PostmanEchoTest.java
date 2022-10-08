7dfeff4301b6ae53b17134ee2f6e302b1b485865
vera4056 <103599166+vera4056@users.noreply.github.com>
2022-10-08 11:41:46 +0300

    test modified

--- src/test/java/ru/netology/rest/PostmanEchoTest.java
+++ src/test/java/ru/netology/rest/PostmanEchoTest.java
@@ -1,26 +1,31 @@
 package ru.netology.rest;
 
+import io.restassured.module.jsv.JsonSchemaValidator;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
-import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
+
 import static org.hamcrest.core.IsEqual.equalTo;
 
 
- class PostmanEchoTest {
+ class
+ PostmanEchoTest {
    @Test
            void shouldReturnSendData () {
 
   given()
   .baseUri("https://postman-echo.com")
   .contentType("text/plain; charset=UTF-8")
-  .body("some data")
+  .body("data")
   .when()
   .post("/post")
   .then()
   .statusCode(200)
   .body("data",equalTo ("some data"));
 
-}
+
  }
+
+ }
+
 
